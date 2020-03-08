e54e991fe47b988e7b09d99c479ca47d52ac97f8
threadkind <threadkind@gmail.com>
2020-03-08 00:47:05 -0600

    Add styling to posts

--- js/scripts.js
+++ js/scripts.js
@@ -17,27 +17,23 @@ fetch('https://www.googleapis.com/blogger/v3/blogs/1786749668754208198/posts?key
     	let content = post.content
     	console.log(content.slice(0,500))
 
-    	// let src = content.indexOf('src=')
-    	// console.log(`Index of src = ${src}`)
-
     	let img = content.substr(content.indexOf('src=')+5, 200)
     	console.log(img)
 
-    	// let endImg = img.indexOf('"')
-    	// console.log(endImg)
 
     	let imageUrl = img.substr(0, img.indexOf('"'))
     	console.log(`Image URL = ${imageUrl}`)
 
-    	html += `${post.title}
+    	html += `<div class="newPosts">${post.title}
     	Published Date: ${post.published}<br>
-    	<a href="${post.url}"><img src=${imageUrl}></a><br><br>`
+    	<a href="${post.url}"><img src=${imageUrl}></a><br><br></div>`
 
     })
 
     document.getElementById('posts').innerHTML = html
 })
 
+// N A V I G A T I O N -------------------->
 
 document.getElementById('eat').addEventListener('mouseover', () => {
 	document.getElementById('eatNav').style.display = "block"
